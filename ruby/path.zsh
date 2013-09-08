c14ead52af9f746a47d214109e1b7bd49fa41830
Mikko Kokkonen <mikko@mikian.com>
2013-09-08 14:06:33 +0300

    finishing touches for setup and bootstrap dotfiles

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,1 +1,2 @@
 export RBENV_ROOT=/usr/local/var/rbenv
+export PATH="$RBENV_ROOT/.rbenv/shims:$RBENV_ROOT/bin:$PATH"
---
+++ system/keys.zsh
@@ -0,0 +1,2 @@
+# Pipe my public key to my clipboard.
+alias pubkey="cat ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
