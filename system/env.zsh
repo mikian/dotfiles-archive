2a63d3a379ecde6e3418244077630bf7a38be56c
Mikko Kokkonen <mikko@mikian.com>
2013-09-09 11:05:18 +0300

    Use UTF8 everywhere

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,7 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
   export EDITOR='/usr/local/bin/mate -w'
 fi
+
+# Set up languages
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
