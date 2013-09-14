aa76152d63f9f7be822eee102b809df5155a1742
Mikko Kokkonen <mikko@mikian.com>
2013-09-14 13:09:50 +0300

    Added user specific configuration support dot runs dot.sh:s, not anymore install.sh:s

---
+++ homebrew/dot.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+. "$(dirname $0)/apps.sh"
+
+# Install taps
+installed_taps=`brew tap`
+for tap in ${taps[@]:0}
+do
+  if [[ ! "$installed_taps" =~ "$tap" ]]
+  then
+    brew tap $tap
+  fi
+done
+
+# Install homebrew packages
+installed_brews=`brew list`
+for brew in ${brews[@]:0}
+do
+  if [[ ! "$installed_brews" =~ "$brew" ]]
+  then
+    brew install $brew
+  fi
+done
+
+installed_apps=`brew cask list`
+for app in ${apps[@]:0}
+do
+  if [[ ! "$installed_apps" =~ "$app" ]]
+  then
+    brew cask install $app
+  fi
+done
+
+exit 0
