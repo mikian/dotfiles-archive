4095e766681528fd78550dc53070e1b599784af7
Mikko Kokkonen <mikko@mikian.com>
2013-10-25 21:48:49 +0300

    Mavericks fixes

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,2 +1,3 @@
 export RBENV_ROOT=/usr/local/var/rbenv
+export CONFIGURE_OPTS="--with-openssl-dir=`brew --prefix openssl` --without-tcl --without-tk"
 export PATH="$RBENV_ROOT/shims:$RBENV_ROOT/bin:$PATH"
